39e8c0ffa1e16f5974412b83271ac4aa44aeff49
Kate <kaygray521@gmail.com>
2018-09-11 20:54:10 -0500

    changed functions
    main calculator function to give bf number if male or female, display bf range and bmi range in functions

--- script.js
+++ script.js
@@ -4,149 +4,116 @@ window.onload = function(){
                // Just Make sure to return false so that your request will not go the server script
 
   var formId = document.getElementById("calcForm");
-  var obj = {};
+  // var obj = {};
 
 
 
   formId.onsubmit = function(){
 
-    function Log10(X){
-       return( Math.log(X) / Math.log(10) );
-    };
+
     // retrieving input data after submit
-    // debugger;
     var neckNum = document.getElementById("neckInputId").value;
     var hipNum = document.getElementById("hipsInputId").value;
-    // var heightNum = document.getElementById("heightInputId").value;
     var waistNum = document.getElementById("waistInputId").value;
     var dateNum = document.getElementById("dateInputId").value;
     var weightNum = document.getElementById("weightInputId").value;
     var feetNum = document.getElementById("feetInputId").value;
     var inchNum = document.getElementById("inchInputId").value;
     var male = document.getElementById("maleId").value;
+
     var female = document.getElementById("femaleId").value;
     var genderId =document.querySelector('input[name="gender"]:checked').value;
-    console.log(genderId);
-
-
+    // console.log(genderId);
 
     // parsing values
     var neckNum = parseInt(neckNum);
     var hipNum = parseInt(hipNum);
-    // var heightNum = parseInt(heightNum);
     var waistNum = parseInt(waistNum);
-    // var dateNum = parseInt(dateNum);
     var weightNum = parseInt(weightNum);
-
-
 
     var feetNum = parseInt(feetNum);
     var inchNum = parseInt(inchNum);
-    var feetToInch = (feetNum * 12);
-    var feetPlusInch = (feetToInch + inchNum);
-    // console.log(feetPlusInch);
-// checking
-    // console.log(dateNum);
-
-  function Log10(X){
-     return( Math.log(X) / Math.log(10) );
-  };
-
-  function calculate(neck, hip, height, waist){
-    var percentFat = (163.205 * Log10(((waist + hip) - neck)) - 97.684 * Log10(height) - 78.387);
-
-    return percentFat
-  }
+    var heightNum = ((feetNum * 12)+ inchNum);
 
 
-  function calculateBfMale(neck, height, waist){
-    var percentFat = ((86.010 * (Log10(waist - neck))) - (70.041 * (Log10(height))) + 36.76);
-
-    return percentFat
-  };
-
-
-
-
-  if (genderId == "Male"){
-    var MaleBf= calculateBfMale(neckNum, feetPlusInch, waistNum);
-    var bfm = MaleBf.toPrecision(3);
-    console.log("male by is " + bfm);
-  }  else {
-    var calculatedStats = calculate(neckNum, hipNum, feetPlusInch, waistNum);
-  
-    var bf =calculatedStats.toPrecision(3);
+    function findBf(genderId){
+      function Log10(X){
+         return( Math.log(X) / Math.log(10) );
+      };
+      if (genderId == "Male"){
+          var percentFat = ((86.010 * (Log10(waistNum - neckNum))) - (70.041 * (Log10(heightNum))) + 36.76);
+          var bf = percentFat.toPrecision(3);
+          console.log("male by is " + bf);
+          return bf
+      }  else {
+          var percentFat = (163.205 * Log10(((waistNum + hipNum) - neckNum)) - 97.684 * Log10(heightNum) - 78.387);
+          var bf = percentFat.toPrecision(3);
+          console.log("female by is " + bf);
+          return bf
+        };
+      };
+    var bf = findBf(genderId);
     console.log(bf);
-  };
 
+    // var bf = findBmi(genderId);
+    // console.log(bf);
 
-
-
-
-
-  // function calculateBfMale(neck, height, waist){
-  //   var percentFat = ((86.010 * (Log10(waist - neck))) - (70.041 * (Log10(height))) + 36.76);
-  //
-  //   return percentFat
-  // };
-  //
-  // var MaleBf= calculateBfMale(neckNum, feetPlusInch, waistNum);
-  // var bfm = MaleBf.toPrecision(3);
-  // console.log("male by is " + bfm);
-
-
-
-  weightBmi = (weightNum);
-  heightBmi = (feetPlusInch*feetPlusInch);
-  // heightBmi = (heightNum*heightNum);
-
-
-  var bmi = ((weightBmi / heightBmi) * 703);
-  // var BMI=weightBmi/Math.pow(heightBmi,2);
+  var bmi = ((weightNum / (heightNum*heightNum)) * 703);
   var bmi = bmi.toPrecision(3);
-  // console.log(bmi);
-
-
-
-  var calculatedStats = calculate(neckNum, hipNum, feetPlusInch, waistNum);
-  // var calculatedStats = calculate(neckNum, hipNum, heightNum, waistNum);
-  // console.log(calculatedStats);
-
-  var bf =calculatedStats.toPrecision(3);
-  console.log(bf);
-
-
-
+  document.getElementById("displayBmi").innerHTML = bmi;
+  document.getElementById("displayInput").innerHTML = bf;
 
   // putting stats in side panel
 
-  document.getElementById("displayInput").innerHTML = bf;
-  // document.getElementById("displayBfRange").innerHTML = "20";
-  document.getElementById("displayBmi").innerHTML = bmi;
-  // document.getElementById("displayBmiRange").innerHTML = "20";
+  function displayBmiRange(bmi){
+    if (bmi<18.5){
+        return "Underweight";
+    }  else if (bmi>=18.5 && bmi<=25){
+        return "Normal"
+    }  else if (bmi>=18.5 && bmi<=25){
+        return "Overwieght";
+    }  else if (bmi>=18.5 && bmi<=25){
+        return "Obese";
+      };
+    };
+  var display = displayBmiRange(bmi);
+  document.getElementById("displayBmiRange").innerHTML = display;
 
-  if (bmi<18.5)
-      document.getElementById("displayBmiRange").innerText = "Underweight";
-    else   if (bmi>=18.5 && bmi<=25)
-      document.getElementById("displayBmiRange").innerText = "Normal";
-   else   if (bmi>=25 && bmi<=30)
-      document.getElementById("displayBmiRange").innerText = "Obese";
-   else   if (bmi>30)
-      document.getElementById("displayBmiRange").innerText = "Overweight";
 
-// womens body fat
-    if (bf<10)
-          document.getElementById("displayBfRange").innerText = "Underfat";
-      else   if (bf>=10 && bf<=13)
-            document.getElementById("displayBfRange").innerText = "Essential Fat";
-      else   if (bf>=13 && bf<=20)
-          document.getElementById("displayBfRange").innerText = "Athletes";
-      else   if (bf>=20 && bf<=25)
-          document.getElementById("displayBfRange").innerText = "Fitness";
-      else   if (bf>=25 && bf<=32)
-              document.getElementById("displayBfRange").innerText = "Average";
-      else   if (bf>32)
-          document.getElementById("displayBfRange").innerText = "Obese";
+  // Bodyfat percentage ranges
+  function bfRanges(bf){
+    if (genderId == "Male"){
+      if (bf<18.5){
+          return "Underfat";
+      }  else if (bf>=3 && bf<=5){
+          return "Essential Fat"
+      }  else if (bf>=5 && bf<=13){
+          return "Athletes";
+      }  else if (bf>=13 && bf<=17){
+          return "Fitness";
+        }  else if (bf>=17 && bf<=24){
+            return "Average";
+          }  else if (bf>24){
+              return "Obese";
+        };
+  }  else {
+    if (bf<10){
+        return "Underfat";
+    }  else if (bf>=10 && bf<=13){
+        return "Essential Fat"
+    }  else if (bf>=13 && bf<=20){
+        return "Athletes";
+    }  else if (bf>=20 && bf<=25){
+        return "Fitness";
+      }  else if (bf>=25 && bf<=32){
+          return "Average";
+        }  else if (bf>32){
+            return "Obese";
+      };
+  };
+  };
+  var bfRangeOutput = bfRanges(bf);
+  document.getElementById("displayBfRange").innerText = bfRangeOutput;
 
 
 
@@ -165,7 +132,8 @@ window.onload = function(){
 
   var dataDate = dateNum;
   var dataWeight = weightNum;
-  var dataBf = calculatedStats.toPrecision(3);
+  var dataBf = bf;
+  // var dataBf = calculatedStats.toPrecision(3);
 
 
 
@@ -193,4 +161,5 @@ window.onload = function(){
 
 
 
-}
+
+};
