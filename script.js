962d94f375e0e2e9119c23b4b9bfcad815d393fe
Kate <kaygray521@gmail.com>
2018-09-20 13:26:25 -0500

    add chart example

--- script.js
+++ script.js
@@ -2,6 +2,30 @@
 window.onload = function(){
                // Just Make sure to return false so that your request will not go the server script
 
+     var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [{
+                label: "My First dataset",
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45],
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+
+
+
+
+
   var formId = document.getElementById("calcForm");
 
 // edit button for table
