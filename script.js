37413b0e529b6dacdc262e58ed935cdeab630e63
Katherine Kote <kaygray521@gmail.com>
2018-12-10 00:01:15 -0600

    Function
    Corrected BMI ranges

--- script.js
+++ script.js
@@ -127,11 +127,11 @@ window.onload = function () {
 		function displayBmiRange( bmi ) {
 			if ( bmi < 18.5 ) {
 				return "Underweight";
-			} else if ( bmi >= 18.5 && bmi <= 25 ) {
+			} else if ( bmi >= 18.5 && bmi < 25 ) {
 				return "Normal"
-			} else if ( bmi >= 18.5 && bmi <= 25 ) {
+			} else if ( bmi >= 25 && bmi < 30 ) {
 				return "Overweight";
-			} else if ( bmi >= 18.5 && bmi <= 25 ) {
+			} else if ( bmi >= 30 ) {
 				return "Obese";
 			};
 		};
